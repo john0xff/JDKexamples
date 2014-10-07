39ae6d2484e80de57b8e1d8d516d17671728c431
BartBien <phoenixjcam@gmail.com>
2014-10-07 10:42:43 +0200

    Tests with lambda and ways of creating threads.

---
+++ concurrency/com/phoenixjcam/threads/create/CreateThreadByExtends2.java
@@ -0,0 +1,69 @@
+package com.phoenixjcam.threads.create;
+
+public class CreateThreadByExtends2
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Countdown");
+
+		ThreadFrame threadFrame = new ThreadFrame("masterTh 1");
+		threadFrame.start();
+		
+		ThreadFrame threadFrame2 = new ThreadFrame("kingTh   2");
+		threadFrame2.start();
+		
+		ThreadFrame threadFrame3 = new ThreadFrame("testTh   3");
+		threadFrame3.start();
+	}
+}
+
+class ThreadFrame extends Thread
+{
+	private Thread thread;
+	private String threadName;
+
+	public ThreadFrame(String threadName)
+	{
+		this.threadName = threadName;
+		
+		if (thread == null)
+		{
+			thread = new Thread(this, threadName);
+		}
+	}
+
+	@Override
+	public void run()
+	{
+		for (int i = 10; i > 0; i--)
+		{
+			String thName = this.thread.getName();
+
+			System.out.println(thName + " count down " + i);
+
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println(this.thread.getName() + " I've done for today.");
+	}
+
+//	@Override
+//	public synchronized void start()
+//	{
+//		if (thread == null)
+//		{
+//			thread = new Thread(this, threadName);
+//
+//			this.thread.start();
+//		}
+//	}
+}
---
+++ concurrency/com/phoenixjcam/threads/create/CreateThreadByRunnable2.java
@@ -0,0 +1,101 @@
+package com.phoenixjcam.threads.create;
+
+class ThreadExample implements Runnable
+{
+	private Thread thread;
+
+	public ThreadExample(String threadName)
+	{
+		thread = new Thread(this, threadName);
+		thread.start();
+	}
+
+	public void run()
+	{
+		for (int i = 10; i > 0; i--)
+		{
+			System.out.println(this.thread.getName() + " counting down " + i);
+
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
+public class CreateThreadByRunnable2
+{
+	private void runnableExample()
+	{
+		new Thread(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					System.out.println("count " + i);
+
+					try
+					{
+						Thread.sleep(100);
+					}
+					catch (InterruptedException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
+		new Thread(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					System.out.println("king " + i);
+
+					try
+					{
+						Thread.sleep(100);
+					}
+					catch (InterruptedException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	public static void main(String[] args)
+	{
+//		new Thread(() -> System.out.println("lambda expression")).start();
+//		
+//		new Thread(new Runnable()
+//		{
+//			@Override
+//			public void run()
+//			{
+//				System.out.println("ordinary runnable");
+//			}
+//		}).start();
+
+		ThreadExample thExmp = new ThreadExample("master");
+		
+		
+	}
+}
