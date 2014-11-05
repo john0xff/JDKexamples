36624639106e609f11f3c853d8a1be225f8643ad
BartBien <phoenixjcam@gmail.com>
2014-11-05 13:09:55 +0100

    ++ buffered in java

---
+++ buffered/Buffered.java
@@ -0,0 +1,18 @@
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+
+public class Buffered
+{
+	public static void mian(String[] args)
+	{
+		BufferedImage bufferedImage;
+		
+		// NIO
+		Buffer buffer;
+		
+		String string;
+		StringBuilder stringBuilder;
+		StringBuffer stringBuffer;
+	}
+}
