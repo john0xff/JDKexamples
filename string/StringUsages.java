e85fc89434142d5416432741df08da11bc51e76f
BartBien <phoenixjcam@gmail.com>
2014-11-05 13:25:35 +0100

    ++ string update

--- string/StringUsages.java
+++ string/StringUsages.java
@@ -5,16 +5,34 @@ import java.util.StringJoiner;
 
 public class StringUsages
 {
-	public static void mian(String[] args)
+	public static void main(String[] args)
 	{
 		// --------------------
 		String string;
 		StringJoiner stringJoiner;
 		
-		// --------------------
-		StringBuilder stringBuilder;
+		String strTmp = "test1";
+		strTmp += "test2";
+		System.out.println("Plain string appended via += -> " + strTmp);
+		
+		boolean bool = strTmp.matches("test1test2");
+		
+		String strToJoin1 = "test1";
+		String strToJoin2 = "test2";
+		String strJoined = String.join("", strToJoin1, strToJoin2);
+		
+		System.out.println("strJoined -> " + strJoined);
 		
 		// --------------------
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append("test1");
+		stringBuilder.append("test2");
+		
+		System.out.println("StringBuilder.append -> " + strJoined);
+		
+		
+		// -------------------- same as StringBuilder but with synchronized methods
 		StringBuffer stringBuffer;
 	}
 }
