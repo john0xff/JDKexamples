e24f778f140933c67f422300283f18c7f3e9a7b5
BartBien <phoenixjcam@gmail.com>
2014-11-05 14:19:04 +0100

    ++ exceptions update

---
+++ exception/throwEx/NullPointerEx.java
@@ -0,0 +1,29 @@
+package throwEx;
+
+import java.util.ArrayList;
+
+public class NullPointerEx
+{
+	@SuppressWarnings("finally")
+	public static void main(String... args)
+	{
+		try
+		{
+			ArrayList<String> c = null;
+			c.get(2);
+		}
+		catch (NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			throw new NullPointerException();
+		}
+	}
+}
+
---
+++ exception/throwEx/ThrowEx.java
@@ -0,0 +1,48 @@
+package throwEx;
+
+import java.util.EmptyStackException;
+
+/**
+ * http://docs.oracle.com/javase/tutorial/essential/exceptions/throwing.html
+ * 
+ * @author BartBien
+ *
+ */
+public class ThrowEx
+{
+	public static void main(String[] args)
+	{
+		pop();
+	}
+	
+	
+
+	private static int size = 0;
+
+	public static Object pop()
+	{
+		Object obj;
+
+		if (size == 0)
+		{
+			throw new EmptyStackException();
+		}
+
+		obj = objectAt(size - 1);
+		setObjectAt(size - 1, null);
+		size--;
+		return obj;
+	}
+
+	private static void setObjectAt(int i, Object object)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	private static Object objectAt(int i)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
