5cc0dca544df12af6aef55968f5026df18f5279c
BartBien <phoenixjcam@gmail.com>
2014-11-07 07:59:57 +0100

    ++ AWT, SWING

---
+++ gui-AWT/SWING/awt/AWT.java
@@ -0,0 +1,13 @@
+package awt;
+
+import java.awt.Frame;
+
+
+public class AWT
+{
+	Frame frame;
+	
+	// Container
+	// http://www.docjar.com/html/api/sun/awt/SunGraphicsCallback.java.html
+	// GraphicsCallback.PrintCallback.getInstance().runComponents(getComponentsSync(), g, GraphicsCallback.LIGHTWEIGHTS);
+}
---
+++ gui-AWT/SWING/swing/SWING.java
@@ -0,0 +1,8 @@
+package swing;
+
+import javax.swing.JFrame;
+
+public class SWING
+{
+	JFrame jFrame;
+}
