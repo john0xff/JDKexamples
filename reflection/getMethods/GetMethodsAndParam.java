d43c98b4dd169abe4fc7f51de432b32ed14541a8
BartBien <phoenixjcam@gmail.com>
2014-11-07 10:35:01 +0100

    ++ reflect get annotation and params

--- reflection/getMethods/GetMethodsAndParam.java
+++ reflection/getMethods/GetMethodsAndParam.java
@@ -7,27 +7,17 @@ import java.util.Map;
 
 import swing.SWING;
 
-public class GetMethods
+public class GetMethodsAndParam
 {
 	static Method[] methods = Thread.class.getMethods();
 
 	public static void main(String... args)
 	{
-		for (Method method : Thread.class.getMethods())
+		for (Method method : ServerSocket.class.getMethods())
 		{
 			System.out.println("method = " + method.getName());
+			System.out.println("number of param = " + method.getParameterCount());
 		}
 		
-		System.out.println(int.class);
-		
-		System.out.println(double.class);
-	
-		System.out.println(String.class);
-		
-		System.out.println(SWING.class);
-		
-		System.out.println(Integer.class);
-		
-		System.out.println(ServerSocket.class);
 	}
 }
