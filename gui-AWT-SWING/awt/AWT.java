3171ebb32ea5a4dc44b3bba13b5baa954d734804
BartBien <phoenixjcam@gmail.com>
2014-11-08 06:59:34 +0100

    ++ gui - AWT, SWING

--- gui-AWT-SWING/awt/AWT.java
+++ gui-AWT-SWING/awt/AWT.java
@@ -10,4 +10,10 @@ public class AWT
 	// Container
 	// http://www.docjar.com/html/api/sun/awt/SunGraphicsCallback.java.html
 	// GraphicsCallback.PrintCallback.getInstance().runComponents(getComponentsSync(), g, GraphicsCallback.LIGHTWEIGHTS);
+	
+	// EventQueue
+	
+	// 1 Thread GUI application - why is that :) - Concurrency in practice.
+	
+	
 }
