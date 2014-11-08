bef899b99ca870cb36af4039b1ed0dee0fabec37
BartBien <phoenixjcam@gmail.com>
2014-11-08 12:29:42 +0100

    streams ascii

---
+++ streams/ASCII/PrintAscii.java
@@ -0,0 +1,17 @@
+package ASCII;
+
+class PrintAscii
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("List of ASCII Characters are given below: ");
+
+		for (int i = 0; i <= 255; i++)
+		{
+			System.out.format("%1$-5d", i);
+			System.out.format("%1$-2c", (char) i);
+			System.out.println();
+		}
+	}
+}
