7d8470746efa69eb268c3efe3ce3a35d811ea2e7
BartBien <phoenixjcam@gmail.com>
2014-11-11 12:47:56 +0100

    base 64

---
+++ base64/com/phoenixjcam/base64/Base64.java
@@ -0,0 +1,21 @@
+package com.phoenixjcam.base64;
+
+/**
+ * 
+ * @author Bart88
+ *
+ */
+public class Base64
+{
+	// mvn
+	// <dependency>
+	// <groupId>javax.mail</groupId>
+	// <artifactId>mail</artifactId>
+	// <version>1.4</version>
+	// </dependency>
+
+	//
+	// javax
+	// com.sun.mail.util.BASE64EncoderStream e;
+	// com.sun.mail.util.BASE64DecoderStream d;
+}
