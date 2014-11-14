17e0486e53d5c031a22f8f25a38bd617476866f7
BartBien <phoenixjcam@gmail.com>
2014-11-14 12:48:08 +0100

    ++ networking update - apche php server connection with java socket + get and print html file

---
+++ networkingTransportLayer/apache/client/url/ApacheGetHTMLwithSocket.java
@@ -0,0 +1,58 @@
+package apache.client.url;
+import java.net.*;
+import java.io.*;
+
+public class ApacheGetHTMLwithSocket
+{
+
+	public static void main(String[] args)
+	{
+
+		int port = 80;
+
+		try
+		{
+			URL u = new URL("http://phoenixjcam.no-ip.biz/index.html");
+
+			if (u.getPort() != -1)
+				port = u.getPort();
+
+			if (!(u.getProtocol().equalsIgnoreCase("http")))
+			{
+				System.err.println("Sorry. I only understand http.");
+				
+			}
+			
+			Socket s = new Socket(u.getHost(), port);
+			OutputStream theOutput = s.getOutputStream();
+			// no auto-flushing
+			PrintWriter pw = new PrintWriter(theOutput, false);
+			// native line endings are uncertain so add them manually
+			pw.print("GET " + u.getFile() + " HTTP/1.0\r\n");
+			pw.print("Accept: text/plain, text/html, text/*\r\n");
+			pw.print("\r\n");
+			pw.flush();
+			InputStream in = s.getInputStream();
+			InputStreamReader isr = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(isr);
+//			int c;
+//			while ((c = br.read()) != -1)
+//			{
+//				System.out.print((char) c);
+//			}
+			String line;
+			while((line = br.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+			
+			
+		}
+		catch (IOException ex)
+		{
+			System.err.println(ex);
+		}
+
+	}
+
+}
