3a51449e8f3ecd89de138648cd36ffa4897a0e5b
BartBien <phoenixjcam@gmail.com>
2014-11-15 23:29:08 +0100

    ++ reflection samples

---
+++ reflection/oracle/ClassDemo.java
@@ -0,0 +1,28 @@
+package oracle;
+
+import java.lang.reflect.Field;
+
+public class ClassDemo
+{
+
+	public static void main(String[] args)
+	{
+
+		try
+		{
+			Class cls = Class.forName("java.awt.Label");
+			System.out.println("Fields =");
+
+			// returns the array of Field objects representing the public fields
+			Field f[] = cls.getFields();
+			for (int i = 0; i < f.length; i++)
+			{
+				System.out.println(f[i]);
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Exception: " + e);
+		}
+	}
+}
